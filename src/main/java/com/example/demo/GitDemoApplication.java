e8b440e6e4530bdcc724b9214774bca67c665c80
narkhedepratik <pratiknarkhede039@gmail.com>
2023-12-15 13:20:29 +0530

    first commit

---
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+	
+	@GetMapping(value = "/welcome")
+	public String getWelcomeMsg() {
+		return "Welcome to parkar digital";
+	}
+
+}
